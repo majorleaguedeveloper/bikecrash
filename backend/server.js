51d542f2cc516acc42d52e0644560b95cfd7b4a5
Kevin <kevin@majorleauguedevelopers.com>
2024-08-01 21:47:27 +0300

    build path

--- backend/server.js
+++ backend/server.js
@@ -40,10 +40,13 @@ io.on('connection', (socket) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-);
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// Handle all GET requests to serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 
 server.listen(4000, () => {
   console.log('Server is running on port 4000');
